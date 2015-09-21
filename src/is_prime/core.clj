576fff786b7b13ff6275f8c567cd6d27b8b345ac
fardog <nate@milkandtang.com>
2015-09-20 19:23:51 -0700

    Move test loop to its own function

--- src/is_prime/core.clj
+++ src/is_prime/core.clj
@@ -1,13 +1,18 @@
 (ns is-prime.core)
 
+(defn- test-prime
+  "Determine if a number is prime by looping through divisors"
+  [x]
+  (loop [n (Math/floor (Math/sqrt x))]
+    (if (= n 1.0)
+      true
+      (if (= 0.0 (mod x n))
+        false
+        (recur (dec n))))))
+
 (defn is-prime
   "Determines if a given integer is prime."
   [x]
   (if (< x 2)
     false
-    (loop [n (Math/floor (Math/sqrt x))]
-      (if (= n 1.0)
-        true
-        (if (= 0.0 (mod x n))
-          false
-          (recur (dec n)))))))
+    (test-prime x)))
