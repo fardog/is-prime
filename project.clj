2b58bf249053059efdbf058aa34ac944e0789b02
fardog <nate@milkandtang.com>
2015-09-20 18:51:45 -0700

    Updates project file

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject is-prime "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Determine if a given number is prime"
+  :url "http://github.com/fardog/is-prime"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]])
